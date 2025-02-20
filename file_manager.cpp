37702ae5d5d327024abdd345320bdc0703661c07
deucenn <panix.black@web.de>
2025-02-20 14:56:11 +0100

    chore: improved readability

--- file_manager.cpp
+++ file_manager.cpp
@@ -3,96 +3,105 @@
 #include <string>
 #include <limits>
 
+void createFile();
+void openFile();
+void deleteFile();
+
 int main()
 {
-    std::cout << "File Manager" << std::endl;
-    std::cout << "1. Create a new file" << std::endl;
-    std::cout << "2. Open an existing file" << std::endl;
-    std::cout << "3. Delete file" << std::endl;
-    std::cout << "4. Exit" << std::endl;
-
     int choice;
-    std::cout << "Enter your choice: ";
-    std::cin >> choice;
+    do {
+        std::cout << "File Manager" << std::endl;
+        std::cout << "1. Create a new file" << std::endl;
+        std::cout << "2. Open an existing file" << std::endl;
+        std::cout << "3. Delete file" << std::endl;
+        std::cout << "4. Exit" << std::endl;
+        std::cout << "Enter your choice: ";
+        std::cin >> choice;
 
-    if (choice == 1)
-    {
-        std::cout << "Creating a new file..." << std::endl;
-        std::string file_name;
-        std::cout << "Enter the name of the file (inlcuding extensions) WARNING: can override files: " << std::endl;
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        std::getline(std::cin, file_name);
-
-        std::ofstream file(file_name, std::ios::trunc);
-
-        if (file.is_open())
-        {
-            std::cout << "Enter the content of the file (enter '***END***' on a new line to save):\n"
-                      << std::endl;
-            std::string line;
-
-            while (std::getline(std::cin, line) && line != "***END***")
-            {
-                file << line << std::endl;
-            }
-
-            file.close();
-            std::cout << "File '" << file_name << "' created successfully" << std::endl;
+        switch (choice) {
+            case 1: createFile(); break;
+            case 2: openFile(); break;
+            case 3: deleteFile(); break;
+            case 4: std::cout << "Exiting..." << std::endl; break;
+            default: std::cout << "Invalid choice. Please try again." << std::endl;
         }
-        else
-        {
-            std::cerr << "Error: Could not create file '" << file_name << "'." << std::endl;
-        }
-    }
-    else if (choice == 2)
-    {
-        std::cout << "Opening an existing file..." << std::endl;
-        std::cout << "Enter the name of the file (inlcuding extensions): " << std::endl;
-        std::string file_name;
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        std::getline(std::cin, file_name);
+    } while (choice != 4);
 
-        std::ifstream file(file_name);
+    return 0;
+}
+
+void createFile() {
+    std::cout << "Creating a new file..." << std::endl;
+    std::string file_name;
+    std::cout << "Enter the name of the file (inlcuding extensions) WARNING: can override files: " << std::endl;
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::getline(std::cin, file_name);
+    
+    std::ofstream file(file_name, std::ios::trunc);
+    
+    if (file.is_open())
+    {
+        std::cout << "Enter the content of the file (enter '***END***' on a new line to save):\n"
+                  << std::endl;
         std::string line;
-
-        if (file.is_open())
+    
+        while (std::getline(std::cin, line) && line != "***END***")
         {
-            while (getline(file, line))
-            {
-                std::cout << line << '\n';
-            }
-            file.close();
+            file << line << std::endl;
         }
-
-        else
-            std::cout << "Unable to open file '" << file_name << "'." << std::endl;
-    }
-    else if (choice == 3)
-    {
-        std::cout << "Deleting file..." << std::endl;
-        std::cout << "Enter the name of the file (inlcuding extensions): " << std::endl;
-        std::string file_name;
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        std::getline(std::cin, file_name);
-
-        if (remove(file_name.c_str()) != 0)
-        {
-            std::cout << "Error deleting file" << std::endl;
-        }
-        else
-        {
-            std::cout << "File '" << file_name << "' successfully deleted" << std::endl;
-        }
-    }
-    else if (choice == 4)
-    {
-        std::cout << "Exiting the program..." << std::endl;
-        return 0;
+    
+        file.close();
+        std::cout << "File '" << file_name << "' created successfully" << std::endl;
     }
     else
     {
-        std::cout << "Invalid choice. Please try again." << std::endl;
-        main();
+        std::cerr << "Error: Could not create file '" << file_name << "'." << std::endl;
     }
-    return 0;
+
+};
+
+void openFile() {
+    std::cout << "Opening an existing file..." << std::endl;
+    std::cout << "Enter the name of the file (inlcuding extensions): " << std::endl;
+    std::string file_name;
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::getline(std::cin, file_name);
+    
+    std::ifstream file(file_name);
+    std::string line;
+    
+    if (file.is_open())
+    {
+        while (getline(file, line))
+        {
+            std::cout << line << '\n';
+        }
+        file.close();
+    }
+    
+    else
+        std::cout << "Unable to open file '" << file_name << "'." << std::endl;
+};
+
+void deleteFile() {
+    std::cout << "Deleting file..." << std::endl;
+    std::cout << "Enter the name of the file (inlcuding extensions): " << std::endl;
+    std::string file_name;
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::getline(std::cin, file_name);
+    
+    if (remove(file_name.c_str()) != 0)
+    {
+        std::cout << "Error deleting file" << std::endl;
+    }
+    else
+    {
+        std::cout << "File '" << file_name << "' successfully deleted" << std::endl;
+    }
 }
+
+
+
+
+
